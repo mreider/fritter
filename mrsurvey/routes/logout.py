1cef89eb16863994313ac869a879f448544e22d8
Alex Xela <alp@steppechange.com>
2015-08-31 18:43:38 +0300

    Fixed flash produced by logout endpoint

--- mrsurvey/routes/logout.py
+++ mrsurvey/routes/logout.py
@@ -1,9 +1,8 @@
 ﻿# -*- coding: utf-8 -*-
 
 from flask import redirect, url_for, current_app
-from flask.ext.login import login_required, logout_user
+from flask.ext.login import logout_user
 
-@login_required
 def logout():
     current_app.logger.info('logout')
     logout_user()
